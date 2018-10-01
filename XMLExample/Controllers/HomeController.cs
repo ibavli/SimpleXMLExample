6edecf1e50e2f6eeb39b48517d74f7d350539b85
ibavli <ibrahim.bavli35@gmail.com>
2018-10-02 00:17:33 +0300

    Last update

--- XMLExample/Controllers/HomeController.cs
+++ XMLExample/Controllers/HomeController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using XMLExample.Models;
 
 namespace XMLExample.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
-            return View();
+            XmlDocument doc = new XmlDocument();
+            List<StudentModel> students = new List<StudentModel>();
+            //doc.Load(Server.MapPath("~/Files/Students.xml"));
+            doc.Load(Server.MapPath("~/Files/StudentsEx.xml"));
+            foreach (XmlNode node in doc.SelectNodes("/students/student"))
+            {
+                students.Add(new StudentModel
+                {
+                    Name = node["name"].InnerText,
+                    Surname = node["surname"].InnerText,
+                    Age = Convert.ToInt32(node["age"].InnerText)
+                });
+            }
+            return View(students);
         }
     }
 }
